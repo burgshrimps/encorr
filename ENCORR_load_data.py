e15879a19893ccb5627ccb6595be3e2cfaa0e63a
alan27 <alan27@mi.fu-berlin.de>
2019-10-30 01:12:04 +0100

    adjust readin in data for LE84

--- ENCORR_load_data.py
+++ ENCORR_load_data.py
@@ -30,17 +30,18 @@ class Parameters:
 def loadparams(params_mat, cut_time_before_stim, cut_time_exp_after_resp):
     param = loadmat(params_mat)
     tetrodes = np.array([tet[0] for tet in param['tet_list'][0]]) 
-    labels = np.array([label[0] for label in param['label_oldnew']]) 
+    #labels = np.array([label[0] for label in param['label_oldnew']]) 
+    labels = np.array(param['label_oldnew'][0]) ### changed for LE84
     stim_study = np.array([stim[0] for stim in param['stim_list_study']]) 
     stim_exp =  param['stim_list_exp'][0] 
     stim_exp_new = stim_exp[np.where(labels == 1)] 
     stim_exp_old = stim_exp[np.where(labels == 0)] 
-    ts_resp_exp = param['ts_response_video'][:,3] * 1000 ### chnaged for LE84, change back to [:,3] for LE46
+    ts_resp_exp = param['ts_response_video'][:,0] * 1000 ### chnaged for LE84, change back to [:,3] for LE46
     ts_resp_exp_new = ts_resp_exp[np.where(labels == 1)] 
     ts_resp_exp_old = ts_resp_exp[np.where(labels == 0)] 
-    ts_stim_all = param['ts_stimon'][0] 
-    ts_stim_study = ts_stim_all[0:10] 
-    ts_stim_exp = param['ts_stimon_exp_video'][:,3] 
+    ts_stim_all = [] ### changed for LE84, change back to param['ts_stimon'][0] for LE46
+    ts_stim_study = param['ts_stimon_study'][:,0] ### changed for LE84, change back to ts_stim_all[0:10] for LE46
+    ts_stim_exp = param['ts_stimon_exp'][:,0] ### changed for LE48, change back to param['ts_stimon_exp_video'][:,3] for LE46
     ts_stim_exp_new = ts_stim_exp[np.where(labels == 1)] 
     ts_stim_exp_old = ts_stim_exp[np.where(labels == 0)] 
     cut_time_study_after_stim = np.mean((ts_resp_exp+cut_time_exp_after_resp)-ts_stim_exp, dtype = int) # mean spiketrain of interest (STOI) length in exp phase
